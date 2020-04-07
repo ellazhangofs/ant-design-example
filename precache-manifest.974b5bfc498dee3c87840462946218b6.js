7fb402d4be656bd5da5b38c1efdb96d57ead1409
Ella Zhang <Ella@ofsystems.com>
2020-04-07 16:49:38 +1000

    Updates

---
+++ precache-manifest.974b5bfc498dee3c87840462946218b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "530795e744e2d068d520db385ed62622",
+    "url": "/ellazhangofs/ant-design-example/index.html"
+  },
+  {
+    "revision": "2f4a4aa130541b8cb099",
+    "url": "/ellazhangofs/ant-design-example/static/css/2.4a9ce227.chunk.css"
+  },
+  {
+    "revision": "cb594d5c807928e4dfcc",
+    "url": "/ellazhangofs/ant-design-example/static/css/main.af35a140.chunk.css"
+  },
+  {
+    "revision": "2f4a4aa130541b8cb099",
+    "url": "/ellazhangofs/ant-design-example/static/js/2.088a242d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ellazhangofs/ant-design-example/static/js/2.088a242d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb594d5c807928e4dfcc",
+    "url": "/ellazhangofs/ant-design-example/static/js/main.40e34023.chunk.js"
+  },
+  {
+    "revision": "5741174b51e15a6cf196",
+    "url": "/ellazhangofs/ant-design-example/static/js/runtime-main.195360cf.js"
+  },
+  {
+    "revision": "8735c1c8e1b85f8e1cde53f71eb3adda",
+    "url": "/ellazhangofs/ant-design-example/static/media/image.8735c1c8.png"
+  }
+]);
